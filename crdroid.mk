987179f63394163b7278154e087dbc2ab88a836d
Benowit <benowit@gmail.com>
2015-09-16 21:46:06 +0200

    crdroid'fy

---
+++ crdroid.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/motorola/victara/full_victara.mk)
+
+# Inherit some common crdroid stuff.
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_RELEASE_NAME := MOTO X (2014)
+PRODUCT_NAME := crdroid_victara
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
